70a51484f9b43a2b29bd94788ff048b700866689
Your name <bhhoang1203@gmail.com>
2024-04-11 11:48:48 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,154 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link, Outlet, useParams, useNavigate, Navigate  } from 'react-router-dom';
+import { useState } from 'react';
+
+
+const BlogPosts = {
+  'first-blog-post': {
+    title: "First Blog Post" 
+    , description: 'Lorem ipsum dolor sit amet, consectetur adip.' },
+  'second-blog-post': {
+    title: "Second Blog Post"
+    , description: "Hello React Router v6"
+  }
+};
+
+function Posts() {
+  return (
+    <div style={{ padding: 20 }}>
+    <h2>Blog</h2>
+    <Outlet/>
+    </div>
+  );
+}
+
+function PostLists() {
+  return (
+    <ul>
+    {Object.entries(BlogPosts).map(([slug, { title }]) => (
+      <li key={slug}>
+      <h3>{title}</h3>
+      </li> 
+    ))}
+    </ul>
+  );
+}
+
+function Post() {
+  const { slug } = useParams();
+  const post = BlogPosts[slug];
+  if(!post) {
+    return <span>The blog post you've requested doesn't exist.</span>; 
+  }
+  const { title, description } = post;
+  return (
+    <div style={{ padding: 20 }}> <h3>{title}</h3> <p>{description}
+    </p> </div>
+  );
+}
+
+function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+    <h2>Home View</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+    <h2>About View</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function NoMatch() {
+  return (
+    <div style={{ padding: 20 }}>
+    <h2>404: Page Not Found</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function Stats({ user }) {
+  if(!user) {
+    return ( <Navigate to="/login" replace />);
+  }
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Stats View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function Login({ onLogin }) {
+  const [creds, setCreds] = useState({});
+  const navigate = useNavigate();
+  function handleLogin() {
+    if(creds.username === 'admin' && creds.password === '123') {
+      onLogin && onLogin({username: creds.username});
+      navigate('/stats');
+    }
+  }
+  return (
+    <div style={{ padding: 10 }}>
+      <br/>
+      <span>Username:</span> <br/>
+      <input type="text" onChange={(e) => setCreds({...creds, username: e.target.value})}/> <br/>
+      <span>Password:</span> <br/>
+      <input type='password' onChange={(e) => setCreds({...creds, password: e.target.value})} /> <br/><br/>
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
+
+function AppLayout() {
+  const [user, setUser] = useState();
+  const navigate = useNavigate();
+
+  function logOut() {
+    setUser(null);
+    navigate("/");
+  }
+
+  return (
+    <>
+    <nav style={{ margin: 10 }}>
+      <Link to="/" style={{ padding: 5 }}> Home </Link>
+      <Link to="/posts" style={{ padding: 5 }}> Posts </Link>
+      <Link to="/about" style={{ padding: 5 }}> About </Link>
+      <span> | </span>
+      { user && <Link to="/stats" style={{ padding: 5 }}> Stats </Link> }
+      { !user && <Link to="/login" style={{ padding: 5 }}> Login </Link> }
+      { user && <span onClick={logOut} style={{ padding: 5, cursor: 'pointer'}}> Logout </span>}
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/posts" element={<Posts />} >
+        <Route index element={<PostLists />} />
+        <Route path=":slug" element={<Post />} />
+      </Route> 
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login onLogin={setUser}/>} />
+      <Route path="/stats" element={<Stats user={user}/>} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
+    </>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <AppLayout />
+    </Router>
   );
 }
 
